360cc009f0d8a4b61cfeb9bf8b6613a708631a86
eugesuri <140730354+eugesuri@users.noreply.github.com>
2023-12-20 16:56:07 +0100

    added search engine

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Dictionary.css";
 
 export default function Dictionary() {
+  let [keyword, setKeyword] = useState("");
+
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
+
+  function search(event) {
+    event.preventDefault();
+    alert(`Searching for ${keyword} definition`);
+  }
   return (
     <div className="Dictionary">
       {" "}
-      <form>
-        <input type="Search" />
+      <form onSubmit={search}>
+        <input type="search" onChange={handleKeywordChange} />
       </form>{" "}
     </div>
   );
